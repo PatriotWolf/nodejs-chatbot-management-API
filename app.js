065bdb826b24c31b7a7cf3b20b206117e2fb88a5
syah <syahfisal37@yahoo.com>
2017-12-15 17:55:52 +0800

    'init'

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/iica',function(req,res){
 	var options = { method: 'POST',
   					url: 'http://13.76.181.19:8080/api/message',
+  					headers: {'content-type': 'text/plain' },
   					body: req.body.msg 
   					};
 
