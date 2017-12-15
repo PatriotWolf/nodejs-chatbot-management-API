8dfa745db4ba662fc3389d4563b54bf8e13ce360
syah <syahfisal37@yahoo.com>
2017-12-15 17:33:28 +0800

    'init'

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+var bodyParser = require('body-parser');
 const app = express()
 const port= process.env.PORT||3000
+
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/iica'(req,res)=>{
 	res.send(req.body)
