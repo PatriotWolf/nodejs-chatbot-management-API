410ee6b8db1fc19a91e99886150f72c3adccbbb2
syah <syahfisal37@yahoo.com>
2017-12-15 17:37:01 +0800

    'init'

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port= process.env.PORT||3000
 
 app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/iica',(req,res)=>{
-	res.send(req)
+app.post('/iica',function(req,res){
+	res.send(req.body)
 })
 app.listen(port, () => console.log('Example app listening on port 3000!'))
