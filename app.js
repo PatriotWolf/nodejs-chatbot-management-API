164a79e176d29a1d21966e8f0839b4583d760dcc
syah <syahfisal37@yahoo.com>
2017-12-15 17:28:47 +0800

    'init'

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ const express = require('express')
 const app = express()
 const port= process.env.PORT||3000
 app.get('/', (req, res) => res.send('Hello World!'))
-
+app.post('/iica'(req,res)=>{
+	res.send(req.body)
+})
 app.listen(port, () => console.log('Example app listening on port 3000!'))
